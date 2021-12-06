b562a0df3d743a2e1eb04d72107d454a3eb9953b
wpiment1 <winifer.pimentel@gmail.com>
2021-12-06 10:52:54 -0500

    Update script.js

--- Updated Weather Project/src/script.js
+++ Updated Weather Project/src/script.js
@@ -1,27 +1,27 @@
-function getCityAndTemp(response) {
-  //changes html to be inputed city and the associated country
-  let searchedCity = response.data.name;
-  let searchedCityCountry = response.data.sys.country;
+// function getCityAndTemp(response) {
+//   //changes html to be inputed city and the associated country
+//   let searchedCity = response.data.name;
+//   let searchedCityCountry = response.data.sys.country;
 
-  let currentCity = document.querySelector("#city-country");
-  currentCity.innerHTML = `${searchedCity}, ${searchedCityCountry}`;
+//   let currentCity = document.querySelector("#city-country");
+//   currentCity.innerHTML = `${searchedCity}, ${searchedCityCountry}`;
 
-  let cityTemp = Math.round(response.data.main.temp);
-  let currentTemp = document.querySelector(".temperature");
-  currentTemp.innerHTML = `${cityTemp}°F`;
-}
+//   let cityTemp = Math.round(response.data.main.temp);
+//   let currentTemp = document.querySelector(".temperature");
+//   currentTemp.innerHTML = `${cityTemp}°F`;
+// }
 
-function changeCity(event) {
-  //gets inputed city
-  event.preventDefault();
-  let cityInput = document.querySelector("#search-input");
-  let searchedCity = cityInput.value;
+// function changeCity(event) {
+//   //gets inputed city
+//   event.preventDefault();
+//   let cityInput = document.querySelector("#search-input");
+//   let searchedCity = cityInput.value;
 
-  let unit = `imperial`;
-  let apiKey = `3c0824dc32f687bca423dec021170f3a`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(getCityAndTemp);
-}
+//   let unit = `imperial`;
+//   let apiKey = `3c0824dc32f687bca423dec021170f3a`;
+//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=${apiKey}&units=${unit}`;
+//   axios.get(apiUrl).then(getCityAndTemp);
+// }
 
 // function showPosition(position) {
 //   let lat = position.coords.latitude;
@@ -38,8 +38,8 @@ function changeCity(event) {
 //   navigator.geolocation.getCurrentPosition(showPosition);
 // }
 
-let citySearch = document.querySelector("#search-form");
-citySearch.addEventListener("submit", changeCity);
+// let citySearch = document.querySelector("#search-form");
+// citySearch.addEventListener("submit", changeCity);
 
 // let currentLocation = document.querySelector(".current-location-button");
 // currentLocation.addEventListener("click", geoLocation);
