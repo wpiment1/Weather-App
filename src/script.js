27b2b40710e7fa4f5bfb21f3c7a43b5cf9215b02
wpiment1 <winifer.pimentel@gmail.com>
2021-12-10 15:09:53 -0500

    working on date and time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,40 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  // if (hours > 12) {
+  //   hours = hours - 12;
+  //   hours = `${hours}`;
+  // }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function getCityWeatherInfo(response) {
   //changes html to be inputed city weather information
   console.log(response);
@@ -22,6 +59,9 @@ function getCityWeatherInfo(response) {
 
   let cityForecast = document.querySelector("#forecast");
   cityForecast.innerHTML = response.data.weather[0].description;
+
+  let currentDate = document.querySelector("#date-time");
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function searchCity(city) {
